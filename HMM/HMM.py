dd83fff70319dc7f8ef54825fc4d5ebe0402d3cb
zhiyong.zhang <zhiyong_zhang1013@163.com>
2018-08-01 18:08:41 +0800

    compute Pr(i_t = q_m | O, A, B, P) and Pr(i_t = q_m, i_{t+1} = q_k | O, A, B, P)

--- HMM/HMM.py
+++ HMM/HMM.py
@@ -47,25 +47,25 @@ class HMM:
         O.append(sample_discrete_ITM(self.B[i, :]))
         return I, O
 
-    def evaluate_forward(self, O):
+    def get_forward_prob(self, O):
         """
         ##
 
-          Given the model and observed samples O, evaluate the probability that the model generating O
+        Given the model (A, B, P) and observed samples O, compute the forward probability
 
         ##
 
         Define the forward probability:
-          f_m(t) = Pr(o_0, o_1, ..., o_t, i_t = q_m | A, B, P), where 0 <= m <= M-1, 0 <= t <= T-1
+          f_t(m) = Pr(o_0, o_1, ..., o_t, i_t = q_m | A, B, P), where 0 <= m <= M-1, 0 <= t <= T-1
         then:
-          f_m(t) = (sum_{k=1}^{M} f_k(t-1) * a_km) * b_m(o_t), where 0 <= m <= M-1, 1 <= t <= T-1.
+          f_t(m) = (sum_{k=1}^{M} f_{t-1}(k) * a_km) * b_m(o_t), where 0 <= m <= M-1, 1 <= t <= T-1.
 
-        Note: f_m(0) = ...
+        Note: f_0(m) = p_m * b_m(o_0) for any 0 <= m <= M-1
 
         ##
 
-        Let f(t) = [f_0(t), f_1(t), ..., f_{M-1}(t)], a row vector, then we have the equation in matrix form:
-          f(t) = (f(t-1) . A) * B[:, o_t] for any 1 <= t <= T-1  --- (eq.1),
+        Let f_t = [f_t(0), f_t(1), ..., f_t(M-1)], a row vector, then we have the equation in matrix form:
+            f_t = (f_{t-1} . A) * B[:, o_t] for any 1 <= t <= T-1  --- (eq.1),
         where "." is matrix multiplication (i.e., "." <=> np.dot())
               "*" multiplies arguments element-wise (i.e., "*" <=> np.multiply())
 
@@ -76,7 +76,7 @@ class HMM:
         """
         T = len(O)
         # forward_prob will be a T x M matrix
-        # the (t, m)-th entry is the forward probability f_m(t)
+        # the (t, m)-th entry is the forward probability f_t(m)
         forward_prob = []
 
         # for np.array: * multiplies arguments element-wise.
@@ -84,32 +84,32 @@ class HMM:
         f0 = self.P * self.B[:, O[0]]
         forward_prob.append(f0)
         for t in range(1, T):
-            # f(t-1)
+            # ft_1 <=> f_{t-1}
             ft_1 = forward_prob[-1]
             # eq.1, matrix form
             ft = np.dot(ft_1, self.A) * self.B[:, O[t]]
             forward_prob.append(ft)
-        return np.sum(forward_prob[-1])
+        return forward_prob
 
-    def evaluate_backward(self, O):
+    def get_backward_prob(self, O):
         """
         ##
 
-          Given the model and observed samples O, evaluate the probability that the model generating O
+        Given the model (A, B, P) and observed samples O, compute the forward probability
 
         ##
 
         Define the backward probability:
-          c_m(t) = Pr(o_{t+1}, o_{t+2}, ..., o_{T-1} | i_t = q_m , A, B, P), where 0 <= m <= M-1, 0 <= t <= T-1
+          c_t(m) = Pr(o_{t+1}, o_{t+2}, ..., o_{T-1} | i_t = q_m , A, B, P), where 0 <= m <= M-1, 0 <= t <= T-1
         then:
-          c_m(t) = sum_{k=1}^{M} (c_k(t+1) * b_k(o_t) * a_mk, where 0 <= m <= M-1, 0 <= t <= T-2.
+          c_t(m) = sum_{k=1}^{M} (c_{t+1}(k) * b_k(o_t) * a_mk, where 0 <= m <= M-1, 0 <= t <= T-2.
 
-        Note: c_m(T-1) = 1 for any 0 <= m <= M-1
+        Note: c_{T-1}(m) = 1 for any 0 <= m <= M-1
 
         ##
 
-        Let c(t) = [c_0(t), c_1(t), ..., c_{M-1}(t)], a row vector, then we have the equation in matrix form:
-          c(t) = A . (c(t+1) * B[:, o_t]), for any 0 <= t <= T-2  --- (eq.2),
+        Let c_t = [c_t(0), c_t(1), ..., c_t(M-1)], a row vector, then we have the equation in matrix form:
+            c_t = A . (c_{t+1} * B[:, o_t]), for any 0 <= t <= T-2  --- (eq.2),
         where "A'" is the transition of matrix A
               "." is matrix multiplication (i.e., "." <=> np.dot())
               "*" multiplies arguments element-wise (i.e., "*" <=> np.multiply())
@@ -124,15 +124,117 @@ class HMM:
         # the (t, m)-th entry is the forward probability c_m(T-1-t)
         backward_prob = []
 
-        # for t = T-1, c_m(t) = 1
+        # for t = T-1, c_t(m) = 1
         backward_prob.append([1] * self.M)
         for t in range(T-2, -1, -1):
-            # c(t+1)
+            # c_t1 <=> c{t+1}
             c_t1 = backward_prob[-1]
             c_t = np.dot(self.A, c_t1 * self.B[:, O[t+1]])
             backward_prob.append(c_t)
-        return np.sum(backward_prob[-1] * self.B[:, O[0]] * self.P)
+        return backward_prob
 
+    def get_single_state_prob(self, O, forward_prob=None, backward_prob=None):
+        """
+        ##
+
+          Given the model (A, B, P) and the observed samples O,
+          evaluate the distribution of a single hidden state at each time,
+          i.e. Pr(i_t = q_m | O, A, B, P) for any 0 <= m <= M-1 and 0 <= t <= T-1
+
+        ##
+
+        Let gamma_t(m) = Pr(i_t = q_m | O, A, B, P)
+                       = Pr(i_t = q_m, O | A, B, P) / Pr(O | A, B, P).
+        As Pr(i_t = q_m, O | A, B, P) = Pr(o_1, o_2, ..., o_t, i_t = q_m, o_{t+1}, ..., o_{T-1} | A, B, P)
+                                      = Pr(o_{t+1}, ..., o_{T-1} | o_1, o_2, ..., o_t, i_t = q_m, A, B, P) * Pr(o_1, o_2, ..., o_t, i_t = q_m | A, B, P)
+                                      = Pr(o_{t+1}, ..., o_{T-1} | i_t = q_m, A, B, P) * f_t(m)
+                                      = c_t(m) * f_t(m)
+        and Pr(O | A, B, P) = sum_{m=0}^{M-1} Pr(i_t = q_m, O | A, B, P) = sum_{m=0}^{M-1} c_t(m) * f_t(m),
+        we have gamma_t(m) = c_t(m) * f_t(m) / (sum_{m=0}^{M-1} c_t(m) * f_t(m))
+
+        ##
+
+            Extra note: If we get the forward probability f_t(m )and the backward probability c_t(m) for any t and m, then
+                  Pr(O | A, B, P) can be evaluated by Pr(O | A, B, P) = sum_{m=0}^{M-1} c_t(m) * f_t(m) with any t!
+
+                  For t = 0:
+                      f_0(m) = p_m * b_m(o_0) for any 0 <= m <= M-1,
+                      we have Pr(O | A, B, P) = sum_{m=0}^{M-1} c_0(m) * b_m(o_0) * f_0(m)  ==>  evaluate_backward()
+
+                  For t = T-1:
+                      c_{T-1}(m) = 1 for any 0 <= m <= M-1,
+                      we have Pr(O | A, B, P) = sum_{m=0}^{M-1} f_{T-1}(m)  ==>  evaluate_forward()
+
+        ##
+        :return:
+        """
+        if not forward_prob:
+            forward_prob = np.array(self.get_forward_prob(O))
+        if not backward_prob:
+            backward_prob = np.array(self.get_backward_prob(O))
+        # "*" is element-wise multiplication
+        return forward_prob * backward_prob / np.sum(forward_prob[-1])
+
+    def get_double_state_prob(self, O, forward_prob=None, backward_prob=None):
+        """
+        ##
+
+          Given the model (A, B, P) and the observed samples O,
+          evaluate the distribution of two hidden states at time t and t+1,
+          i.e. Pr(i_t = q_m, i_{t+1} = q_k | O, A, B, P) for any 0 <= m, k <= M-1 and 0 <= t <= T-2
+
+        ##
+
+        Let xi_t(m, k) = Pr(i_t = q_m, i_{t+1} = q_k | O, A, B, P)
+                       = Pr(i_t = q_m, i_{t+1} = q_k, O | A, B, P) / Pr(O | A, B, P).
+        As Pr(i_t = q_m, i_{t+1} = q_k, O | A, B, P) = Pr(o_1, o_2, ..., o_t, i_t = q_m, i_{t+1} = q_k, o_{t+1}, ..., o_{T-1} | A, B, P)
+                                      = Pr(o_{t+2}, ..., o_{T-1} | i_{t+1} = q_k, A, B, P) * Pr(o_1, o_2, ..., o_t, i_t = q_m, i_{t+1} = q_k, o_{t+1} | A, B, P)
+                                      = c_{t+1}(m) * Pr(o_{t+1} | i_{t+1} = q_k, A, B, P) * Pr(o_1, o_2, ..., o_t, i_t = q_m, i_{t+1} = q_k | A, B, P)
+                                      = c_{t+1}(m) * b_k(o_{t+1}) * Pr(i_{t+1} = q_k | i_t = q_m, A, B, P) * Pr(o_1, o_2, ..., o_t, i_t = q_m | A, B, P)
+                                      = c_{t+1}(m) * b_k(o_{t+1}) * a_mk * f_t(m)
+                                      = f_t(m) * a_mk * b_k(o_{t+1}) * c_{t+1}(m)
+        we have xi_t(m, k) = f_t(m) * a_mk * b_k(o_{t+1}) * c_{t+1}(m) / Pr(O | A, B, P)
+
+        Matrix form:
+            for any 0 <= t <= T-2, f_t(m) * a_mk * b_k(o_{t+1}) * c_{t+1}(m) is obtained by the following operations:
+            1. multiply the m-th row of A by f_t(m): np.array([f_t * A[:, k] for k in range(M)])
+            2. multiply the k-th column of A by b_k(o_{t+1}) * c_{t+1}(m): np.array([B[:, O[t+1]] * c_{t+1} * A[m, :] for m in range(M)])
+
+        ##
+        :return:
+        """
+        if not forward_prob:
+            forward_prob = np.array(self.get_forward_prob(O))
+        if not backward_prob:
+            backward_prob = np.array(self.get_backward_prob(O))
+
+        T = len(O)
+        double_state_prob = []
+        for t in range(T-1):
+            # operate on each column
+            double_state_prob_t = [forward_prob[t, :] * self.A[:, k] for k in range(self.M)]
+            # double_state_prob_t is a list of array
+            double_state_prob_t = [self.B[:, O[t+1]] * backward_prob[t+1, :] * double_state_prob_t[m] for m in range(self.M)]
+            # np.array(list of n-D arrays with the same shape) => (n+1)-D array
+            # double_state_prob is also a list of array
+            double_state_prob.append(np.array(double_state_prob_t))
+        return np.array(double_state_prob)
+
+    def evaluate_forward(self, O):
+        """
+          Given the model (A, B, P) and the observed samples O,
+          evaluate the probability that the model generates O using forward probability
+        """
+        forward_prob = self.get_forward_prob(O)
+        return np.sum(forward_prob[-1])
+
+    def evaluate_backward(self, O):
+        """
+          Given the model (A, B, P) and the observed samples O,
+          evaluate the probability that the model generates O using backward probability
+        """
+        backward_prob = self.get_backward_prob(O)
+        return np.sum(backward_prob[-1] * self.B[:, O[0]] * self.P)
 
     def decoding_Vertebi(self, O):
         """
@@ -222,3 +324,5 @@ if '__main__' == __name__:
     print(hmm_instance.evaluate_forward(out_samples))
     print(hmm_instance.evaluate_backward(out_samples))
     print(hmm_instance.decoding_Vertebi(out_samples))
+    print(hmm_instance.get_single_state_prob(out_samples))
+    print(hmm_instance.get_double_state_prob(out_samples))
